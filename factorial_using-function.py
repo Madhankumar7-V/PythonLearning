743c9668493456655799be78e438c66cece91d79
Madhankumar7-V <damnnwhosthis@gmail.com>
2026-07-31 23:15:23 +0530

    pushed all pattern codes

--- factorial_using-function.py
+++ factorial_using-function.py
@@ -2,3 +2,4 @@ import math
 num=int(input("Enter a Number : "))
 result=math.factorial(num)
 print("Factoial = ", result)
+
---
+++ sqaurePattern.py
@@ -0,0 +1,5 @@
+rows=4
+for i in range(rows):
+    for j in range(rows):
+        print("*", end=" ")
+    print()
