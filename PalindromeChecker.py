629dcba12dc8800442092621d3e8091ec21d2965
Madhankumar7-V <damnnwhosthis@gmail.com>
2026-07-31 22:44:32 +0530

    added few codes

---
+++ PalindromeChecker.py
@@ -0,0 +1,5 @@
+value=input("Enter a String : ")
+if (value==value[::-1]):
+    print(f"{value} is a Palindrome")
+else:
+    print(f"{value} is not a palindrome")
---
+++ reverseNumber.py
@@ -0,0 +1,11 @@
+n=int(input("Enter a Number : "))
+print("Original : ",n)
+reverse=0
+
+while n>0:
+    digit=n%10
+    reverse=reverse*10+digit
+    n//=10
+
+print("Reversed : ",reverse)
+              
